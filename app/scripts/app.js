d1b97952cd3569123ec0cf02ee98b150fa2408b9
tejasrikalluri <tkalluri@artissol.com>
2022-07-25 23:39:08 +0530

    continoution

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -9,16 +9,23 @@ async function init() {
 
 async function renderText() {
   const iparamData = await client.iparams.get();
-  console.log(iparamData)
+  const ticketData = await client.data.get('ticket');
   const { types } = iparamData;
+  const { ticket: { type, custom_fields: { cf_customer } } } = ticketData;
+  let typeMatch = types.indexOf(types);
+  console.log(type, cf_customer, typeMatch)
   console.log("**************************************")
   console.log(types)
+  let  eventCallback = async function (event) {
+    console.log("Update properies happened")
+    const ticketData = await client.data.get('ticket');
+    const { ticket: { custom_fields: { cf_customer } } } = ticketData;
+    console.log(cf_customer)
+    // console.log(cf_customer)
+    event.helper.done();
+    event.helper.fail('errorMessage')
+  };
   client.events.on("ticket.propertiesUpdated", eventCallback, { intercept: true });
 }
-let eventCallback = function (event) {
-  console.log(event.type + " event occurred");
-  var event_data = event.helper.getData();
-  console.log(event_data)
-  event.helper.done()
-  event.helper.fail('errorMessage')
-};
+
+
